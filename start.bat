ee7d1f262b4385c1085a0d2f575dd119081c01b7
DarkSoulxUltra <97820008+DarkSoulxUltra@users.noreply.github.com>
2022-01-31 19:15:29 +0530

    Update start.bat

--- start.bat
+++ start.bat
@@ -1,4 +1,4 @@
 @echo off
 TITLE Unmei Bot
-:: Enables virtual env mode and then starts Flare_Robot
+:: Enables virtual env mode and then starts Destiny_x_Bot
 env\scripts\activate.bat && py -m Unmei_main
